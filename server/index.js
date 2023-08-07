ebb1d2653945e54da666528dc1f372d271f43199
Jon Jackson <jonjacksonvibes@gmail.com>
2023-08-07 12:35:43 -0500

    fixed node modules hopefully

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const colors = require('colors');
-const cors = require('cors');
-require('dotenv').config();
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
-const connectDB = require('./config/db');
+const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5001;
 
 const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 app.use(
   "/graphql",
